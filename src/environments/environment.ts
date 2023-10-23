276aaa173626cf136c4eeeb02cb60b20f987b3c4
benia592 <benialeo592@gmail.com>
2023-10-23 17:09:36 +0200

    gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    //
+    projectId: 'sharemyplaces-54037',
+    appId: '1:534482941142:web:f0531480c23528cd3d4577',
+    storageBucket: 'sharemyplaces-54037.appspot.com',
+    apiKey: 'AIzaSyAYLtCrHnlIVTjC3yhsxnrECPJsEzf-LVU',
+    authDomain: 'sharemyplaces-54037.firebaseapp.com',
+    messagingSenderId: '534482941142',
+    measurementId: 'G-J625XHYHCE',
   }
 }
